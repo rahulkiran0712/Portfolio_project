960371038dc15da235af5f45cba8f78404c02c41
rahulkiran0712 <138213387+rahulkiran0712@users.noreply.github.com>
2023-08-01 15:42:28 +0530

    Update and rename Portfolio_Project1.sql to COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -1,17 +1,27 @@
---SELECT *
---FROM Portfolio..covid_deaths
---ORDER BY 3,4;
+/*
+Covid 19 Data Exploration 
 
---SELECT *
---FROM Portfolio..covid_vaccinations
---ORDER BY 3,4;
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+SELECT *
+FROM Portfolio..covid_vaccinations
+WHERE continent is not NULL	
+ORDER BY 3,4;
 
 /**ALTER table dbo.covid_deaths
 ALTER column total_deaths float
  **/ 
  
- -- Looking at Total_cases vs Total_deaths
- 
+-- Select Data that we are going to be starting with
+Select Location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+order by 1,2
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country	
  SELECT location, date , total_cases, total_deaths, (total_cases / population)*100 AS Death_pecentage
  FROM Portfolio..covid_deaths
  WHERE location LIKE 'India'
@@ -45,7 +55,7 @@ GROUP  BY location,population
 
  -- Lets break thing by continent 
 
-	  SELECT continent, MAX(total_deaths) as	Total_death_count
+	  SELECT continent, MAX(total_deaths) as Total_death_count
 	 FROM Portfolio..covid_deaths
 	 WHERE continent is NOT NULL 
 	 GROUP BY continent
@@ -63,7 +73,7 @@ GROUP  BY location,population
 	 ORDER BY 1,2
 
 
--- Using CTE
+-- Using CTE to perform Calculation on Partition By in previous query
 
 with popvsvac (continent, location, date, population, rollingpeoplevacccinated, new_vaccination)
 as 
